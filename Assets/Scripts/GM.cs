0738084b6cdc385f6449dae2613d651fe18848d9
Craner <iayfnp@gmail.com>
2023-03-30 01:53:17 -0700

    hi

--- Assets/Scripts/GM.cs
+++ Assets/Scripts/GM.cs
@@ -5,27 +5,35 @@ using UnityEngine;
 using static System.Math;
 
 public class GM : MonoBehaviour {
-    //private enum NESW { Up, Right, Down, Left } 
     public GameObject[] prefabs;
+    private GameObject[] objects = new GameObject[16]; // array of scripts (tile script)?
 
     void Start() {
+        init();
+    }
+
+    void Update() {
+        if (Input.GetKeyDown("space"))
+            init();
+    }
+
+    void init() {
+        foreach (GameObject elem in objects)
+            if (elem != null)
+                Destroy(elem);
+
         bool flip = true;
-        int index;
-        index = Random.Range(0, 16);
+        int index = Random.Range(0, 16);
         List<int> history = new List<int> { index };
-
         List<int>[] perms = new List<int>[16];
-        for (int i = 0; i < 16; ++i)
-            perms[i] = new int[] { -4, 1, 4, -1 }.OrderBy(elem => Random.Range(0f, 1f)).ToList();
+        perms[index] = new int[] { -4, 1, 4, -1 }.OrderBy(elem => Random.Range(0f, 1f)).ToList();
 
-        int timer = 0;
-        for (; history.Count < 16; ++timer) {
-            print(timer.ToString() + ": " + history.Count.ToString());
+        while (history.Count < 16) {
+            int backup = index;
             index += perms[index][0];
-            int backup = !flip ? history.Last() : history.First();
             if (0 > index || index >= 16 || Abs(perms[backup][0] % 2) != Abs(backup % 4 - index % 4) || history.Contains(index)) {
                 flip = !flip;
-                history.Insert(flip ? 0 : history.Count - 1, -1);
+                history.Insert(flip ? 0 : history.Count, -1);
 
                 do {
                     history.RemoveAt(flip ? 0 : history.Count - 1);
@@ -47,12 +55,8 @@ public class GM : MonoBehaviour {
                                     hash(4, 1), hash(-1, -4), hash(-1, 4), hash(-4, 1), hash(-4, -1), hash(1, 4), hash(1, -4), hash(4, -1) };
         for (int i = 2; i < 18; ++i) {
             int elem = history[i - 1];
-            Instantiate(prefabs[System.Array.IndexOf(table, hash(elem - history[i - 2], history[i] - elem)) / 2], new Vector3(pos[elem % 4], -pos[elem / 4], 0), Quaternion.identity);
+            objects[i - 2] = Instantiate(prefabs[System.Array.IndexOf(table, hash(elem - history[i - 2], history[i] - elem)) / 2], new Vector3(pos[elem % 4], -pos[elem / 4], 0), Quaternion.identity);
         }
-    }
-
-    void Update() {
-        
     }
 
     int hash(int x, int y) {
